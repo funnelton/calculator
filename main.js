b002f0def1a37f4d994ca974db52a746df25f228
funnelton <57541226+funnelton@users.noreply.github.com>
2024-02-03 12:49:25 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -47,14 +47,12 @@ buttonGetEstimate.addEventListener('click', function () {
     let summary_vehicleModel = document.getElementById('summary_vehicleModel');
     let summary_glassAndMoldingPrice = document.getElementById('summary_glassAndMoldingPrice');
     let summary_replacementPrice = document.getElementById('summary_replacementPrice');
-    let summary_tax = document.getElementById('summary_tax');
     let summary_total = document.getElementById('summary_total');
     
     summary_vehicleModel.textContent = vehicleModel;
     summary_glassAndMoldingPrice.textContent = `Glass & Molding: $${glassAndAccessoriesAndProfitPrice}`;
     summary_replacementPrice.textContent = `Replacement: $${replacementPrice}`;
-    summary_tax.textContent = `Tax: ${consts.tax}%`;
-    summary_total.textContent = `Total: $${total}`;
+    summary_total.textContent = `Total: $${total}(tax included)`;
 
 });
 
